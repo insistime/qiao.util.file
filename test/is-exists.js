4e30b9d05ea4c45f167e2bf7b014989152a1463b
uikoo9 <uikoo9@qq.com>
2018-10-17 17:00:35 +0800

    modify path

--- test/is-exists.js
+++ test/is-exists.js
@@ -3,8 +3,9 @@
 var qiaoUtilFile = require('../lib/qiao.util.file');
 
 var test = function(){
-	var path = 'z:/workspaces/qiao.plugin.coder/lib/qiao.plugin.coder.js';
-	var s = qiaoUtilFile.isExists(path);
+	var fpath 	= 'z:/workspaces/qiao.plugin.coder/lib/qiao.plugin.coder.js';
+	var s		= qiaoUtilFile.isExists(fpath);
+	
 	console.log(s);
 };
 
